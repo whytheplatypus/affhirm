e980f983cc904ba0da16beb78d38660788129df9
David Gage <dgage@fearless.tech>
2023-11-05 16:58:33 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use jsonschema::{is_valid, Draft, JSONSchema};
+use reqwest;
+use serde_json::{Value, json};
+use std::fs;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    //let schema: Value =
+        //reqwest::blocking::get("http://hl7.org/fhir/r4/fhir.schema.json")?.json()?;
+
+    let schema = json!({"$ref": "http://hl7.org/fhir/r5/fhir.schema.json"});
+
+    let compiled = JSONSchema::options()
+        .with_draft(Draft::Draft6)
+        .compile(&schema)
+        .expect("A valid schema");
+
+    let contents =
+        fs::read_to_string("Luci727_Murphy561_fdc21ebe-f012-0831-ccfc-28d243f4b5ac.json")
+            .expect("Should have been able to read the file");
+    let instance: Value = serde_json::from_str(&contents)?;
+    match compiled.validate(&instance) {
+        Ok(()) => {
+            println!("Valid!!")
+        }
+        Err(errors) => {
+            for error in errors {
+                println!("Validation error: {}", error);
+                println!("Instance path: {}", error.instance_path);
+            }
+        }
+    }
+    Ok(())
+}
